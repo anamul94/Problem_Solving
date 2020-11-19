cd4a6a11f2a176f6d24cd1f4760fc0ab824596f7
anamul94 <anamul.ice14@gmail.com>
2020-11-19 15:17:37 +0600

    This is HackerRank Daily Python Task

---
+++ if_else.py
@@ -0,0 +1,10 @@
+import math
+n = 3
+if n%2!=0:
+    print("Weird")
+if n%2==0 and n in range(2,6):
+    print("Not Weird")
+if n%2==0 and n in range(6,21):
+    print("Weird")
+if n%2==0 and n>20:
+    print("Not Weird")
